ef743c9f6fa24bab394f8d10ead20f4bd23aa362
khai96_ <hvksmr1996@gmail.com>
2018-12-22 05:49:19 +0700

    Create empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate gtk;
+
+#[macro_use]
+extern crate relm;
+
+use gtk::prelude::*;
+use relm::Widget;
+
+pub struct Model {}
+
+#[derive(relm_derive::Msg)]
+pub enum Msg {
+    Quit,
+}
+
+#[relm_attributes::widget]
+impl Widget for Win {
+    fn model() -> Model {
+        Model {}
+    }
+
+    fn update(&mut self, msg: Msg) {
+        match msg {
+            Msg::Quit => gtk::main_quit()
+        }
+    }
+
+    view! {
+        gtk::Window {
+            gtk::Box {},
+
+            delete_event(_, _) => (Msg::Quit, gtk::Inhibit(false)),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Win::run(()).unwrap();
 }
