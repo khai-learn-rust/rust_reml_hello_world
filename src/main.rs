b93533b6e1a8138848f9736a7d79b70580e3e109
khai96_ <hvksmr1996@gmail.com>
2018-12-22 12:53:25 +0700

    Populate with things

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,63 @@ extern crate relm;
 use gtk::prelude::*;
 use relm::Widget;
 
-pub struct Model {}
+pub struct Model {
+    count: i32,
+}
 
 #[derive(relm_derive::Msg)]
 pub enum Msg {
+    Decrease,
+    Increase,
     Quit,
 }
 
 #[relm_attributes::widget]
 impl Widget for Win {
     fn model() -> Model {
-        Model {}
+        Model {
+            count: 0,
+        }
     }
 
     fn update(&mut self, msg: Msg) {
         match msg {
-            Msg::Quit => gtk::main_quit()
+            Msg::Decrease => self.model.count -= 1,
+            Msg::Increase => self.model.count += 1,
+            Msg::Quit => gtk::main_quit(),
         }
     }
 
     view! {
         gtk::Window {
-            gtk::Box {},
+            gtk::Box {
+                orientation: gtk::Orientation::Vertical,
+
+                #[name="label"]
+                gtk::Label {
+                    text: &self.model.count.to_string()
+                },
+
+                #[name="button_container"]
+                gtk::Box {
+                    orientation: gtk::Orientation::Horizontal,
+
+                    #[name="dec_button"]
+                    gtk::Button {
+                        clicked => Msg::Decrease,
+                        label: "-",
+                    },
+
+                    #[name="inc_button"]
+                    gtk::Button {
+                        clicked => Msg::Increase,
+                        label: "+",
+                    },
+                }
+            },
 
             delete_event(_, _) => (Msg::Quit, gtk::Inhibit(false)),
+            title: "Simple Counter",
         }
     }
 }
